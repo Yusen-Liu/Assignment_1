38b9fa86a80e49c6c6d402d96c866d44b219f83d
Yusen-Liu <lysen1008@gmail.com>
2017-05-26 16:30:47 +0200

    added second part

---
+++ Complex.java
@@ -0,0 +1,12 @@
+package AS1_3_calculation;
+
+public class Complex {
+ 	double real;   
+    double image;   
+      
+    Complex(double real, double image){  
+       
+      this.real = real;
+      this.image = image;
+    }  
+}
