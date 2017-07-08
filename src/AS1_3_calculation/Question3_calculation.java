5d2ee832ecca9bfff16a046d830066e9d3dd1465
Yusen-Liu <lysen1008@gmail.com>
2017-07-08 18:04:27 +0800

    Fix Y matrix calculations

--- src/AS1_3_calculation/Question3_calculation.java
+++ src/AS1_3_calculation/Question3_calculation.java
@@ -3,6 +3,9 @@ package AS1_3_calculation;
 import java.io.File;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.xml.parsers.DocumentBuilder;
 
 import org.w3c.dom.Document;
@@ -15,16 +18,16 @@ public class Question3_calculation {
 	public static void main(String[] args) {
 		   
 		try {
-			double ZL6r =0, ZL6x = 0, ZL6shr = 0, ZL6shx = 0, L6 = 0, ZL2r = 0, ZL2x = 0 , ZL2shr = 0, ZL2shx = 0, L2 = 0, ZT1r = 0, 
-			ZT1x = 0, ZT2r = 0, ZT2x = 0, ZT3r = 0, ZT3x = 0, V1 = 0, V2 = 0, V3 = 0, V4 = 0, V5 = 0, ZC1r = 0, ZC1x = 0, ZC2r = 0, ZC2x = 0, P123r = 0,
+			double ZL6r =0, ZL6x = 0, YL6shg = 0, YL6shb = 0, L6 = 0, ZL2r = 0, ZL2x = 0 , YL2shg = 0, YL2shb = 0, L2 = 0, ZT1r = 0, 
+			ZT1x = 0, ZT2r = 0, ZT2x = 0, ZT3r = 0, ZT3x = 0, V1 = 0, V2 = 0, V3 = 0, V4 = 0, V6 = 0, YC1g = 0, YC1b = 0, YC2g = 0, YC2b = 0, P123r = 0,
 			P123x = 0, PL2r = 0, PL2x = 0, PL1r = 0, PL1x = 0;
 						
-			File XmlFile = new File("C:/Users/lfs/Desktop/eh2745/Assignment_EQ_reduced.xml");
+			File XmlFile = new File("/Users/Lysen/Documents/computer application/Assignment 1/Assignment_EQ_reduced.xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(XmlFile);
 						 
-			File XmlFile2 = new File("C:/Users/lfs/Desktop/eh2745/Assignment_SSH_reduced.xml");
+			File XmlFile2 = new File("/Users/Lysen/Documents/computer application/Assignment 1/Assignment_SSH_reduced.xml");
 			DocumentBuilderFactory dbFactory2 = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder2 = dbFactory2.newDocumentBuilder();
 			Document doc2 = dBuilder2.parse(XmlFile2);
@@ -52,15 +55,15 @@ public class Question3_calculation {
 			if (i==0){
 				ZL6r = Double.parseDouble(ZLreal);
 				ZL6x = Double.parseDouble(ZLim);
-				ZL6shr = Double.parseDouble(ZLshreal);
-				ZL6shx = Double.parseDouble(ZLshreal);
+				YL6shg = Double.parseDouble(ZLshreal);
+				YL6shb = Double.parseDouble(ZLshreal);
 				L6 = Double.parseDouble(ZLlength);
 				
 			}else{
 				ZL2r = Double.parseDouble(ZLreal);
 				ZL2x = Double.parseDouble(ZLim);
-				ZL2shr = Double.parseDouble(ZLshreal);
-				ZL2shx = Double.parseDouble(ZLshim);
+				YL2shg = Double.parseDouble(ZLshreal);
+				YL2shb = Double.parseDouble(ZLshim);
 				L2 = Double.parseDouble(ZLlength);	
 				}
 				
@@ -88,7 +91,7 @@ public class Question3_calculation {
 							if (i==4){
 								 ZT3r = Double.parseDouble(ZTreal);
 								 ZT3x = Double.parseDouble(ZTim);
-								System.out.println( ZT3r +"\n" + ZT3x +"\n" );
+
 							}
 							else{
 								continue;
@@ -118,14 +121,14 @@ public class Question3_calculation {
 						else{
 							if (i==2){
 								 V3 = Double.parseDouble(busvolt);
-								System.out.println( V3 +"\n" );
+
 							}
 							else{
 								if (i==3){
 									 V2 = Double.parseDouble(busvolt);
 								}
 								else{
-									 V5 = Double.parseDouble(busvolt);
+									 V6 = Double.parseDouble(busvolt);
 								}
 							}
 						}
@@ -143,14 +146,13 @@ public class Question3_calculation {
 
 					
 					if (i==0){
-					 ZC1r = Double.parseDouble(ZCreal);
-					 ZC1x = Double.parseDouble(ZCim);
+					 YC1b = Double.parseDouble(ZCreal);
+					 YC1g = Double.parseDouble(ZCim);
 					
 					}
 					else{
-						 ZC2r = Double.parseDouble(ZCreal);
-						 ZC2x = Double.parseDouble(ZCim);
-						System.out.println( ZC2r +"\n" + ZC2x +"\n");
+						 YC2b = Double.parseDouble(ZCreal);
+						 YC2g = Double.parseDouble(ZCim);
 					}
 				}
 						
@@ -175,47 +177,65 @@ public class Question3_calculation {
 						else{
 								 PL1r = Double.parseDouble(ZLreal);
 								 PL1x = Double.parseDouble(ZLim);
-								System.out.println( PL1r +"\n" + PL1x +"\n" );
+
 							
 						}
 					
 					}
 					}
-				System.out.println( PL1r +"\n" + PL1x +"\n" );
-			        Complex ZC1 = new Complex(ZC1r,ZC1x);
-			        Complex ZC2 = new Complex(ZC2r,ZC2x);
-			        Complex ZT1 = new Complex(ZT1r,ZT1x);
-			        Complex ZT2 = new Complex(ZT2r,ZT2x);
-			        Complex ZT3 = new Complex(ZT3r,ZT3x);
-			        Complex ZL2 = new Complex(L2*ZL2r,L2*ZL2x);
-			        Complex ZL6 = new Complex(L6*ZL6r,L6*ZL6x);
-			        Complex SLD1 = new Complex(PL1r,PL1x);
-			        Complex SLD2 = new Complex(PL2r,PL2x);
-			        Complex SLD123 = new Complex(P123r,P123x);
-			        Complex ZL2sh = new Complex(L2*ZL2shr,L2*ZL2shx);
-			        Complex ZL6sh = new Complex(L6*ZL6shr,L6*ZL6shx);
-			        Complex U5 = new Complex(V5,0);
-			        Complex U2 = new Complex(V2,0);
-			        Complex U3 = new Complex(V3,0);
-			        Complex ref = new Complex(1,0);
-			        Complex refm = new Complex(-1,0);
-			        Complex ZLD1 = div(mul(U5,U5),SLD1);
-			        Complex ZLD2 = div(mul(U3,U3),SLD2);
-			        Complex ZLD123 = div(mul(U2,U2),SLD123);
-			        Complex y[][] = new Complex[5][5];
+
+			        
+				double Sbase = 1000, Zbase1 = 0, Zbase2 = 0, Zbase3 = 0, Zbase4 = 0, Zbase6 = 0;
+				Zbase1 = V1*V1/Sbase;
+				Zbase2 = V2*V2/Sbase;
+				Zbase3 = V3*V3/Sbase;
+				Zbase4 = V4*V4/Sbase;
+				Zbase6 = V6*V6/Sbase;
+				
+				
+
+				Complex YC1 = new Complex(YC1g,YC1b);
+		        Complex YC2 = new Complex(YC2g,YC2b);			       
+			    Complex ZT1 = new Complex(ZT1r/Zbase6,ZT1x/Zbase6);
+			    Complex ZT2 = new Complex(ZT2r/Zbase6,ZT2x/Zbase6);
+			    Complex ZT3 = new Complex(ZT3r/Zbase4,ZT3x/Zbase4);
+			    Complex ZL2 = new Complex(ZL2r/Zbase3,ZL2x/Zbase3);
+			    Complex ZL6 = new Complex(ZL6r/Zbase3,ZL6x/Zbase3);
+			    Complex SLD1 = new Complex(PL1r,PL1x);
+			    Complex SLD2 = new Complex(PL2r,PL2x);
+			    Complex SLD123 = new Complex(P123r,P123x);
+			    Complex YL2sh = new Complex(YL2shg,YL2shb);
+			    Complex YL6sh = new Complex(YL6shg,YL6shb);
+			    Complex U6 = new Complex(V6,0);
+			    Complex U2 = new Complex(V2,0);
+			    Complex U3 = new Complex(V3,0);
+				Complex Z_base1 = new Complex(Zbase1,0);
+			    Complex Z_base6 = new Complex(Zbase6,0);
+			    Complex Z_base3 = new Complex(Zbase3,0);
+			    Complex Z_base2 = new Complex(Zbase2,0);
+			    Complex ref = new Complex(1,0);
+			    Complex refm = new Complex(-1,0);
+			    Complex ZC1 = div(div(ref,YC1),Z_base1);
+			    Complex ZC2 = div(div(ref,YC2),Z_base6);
+			    Complex ZLD1 = div(div(mul(U6,U6),SLD1),Z_base6);
+			    Complex ZLD2 = div(div(mul(U3,U3),SLD2),Z_base3);
+			    Complex ZLD123 = div(div(mul(U2,U2),SLD123),Z_base2);
+			    Complex y[][] = new Complex[5][5];
+			    YL6sh = div(YL6sh, div(ref,Z_base3));
+			    YL2sh = div(YL2sh, div(ref,Z_base3));
 			         y[0][0] = add(div(ref,ZC2),div(ref,ZT1));
 			         y[0][1] = new Complex(0,0);
 			         y[0][2] = new Complex(0,0);
 			         y[0][3] = new Complex(0,0);
 			         y[0][4] = div(refm,ZT1);
 			         y[1][0] = new Complex(0,0);
-			         y[1][1] = add(add(add(div(ref,ZL6),div(ref,ZL6sh)),add(div(ref,ZL2),div(ref,ZL2sh))),div(ref,ZLD123));
+			         y[1][1] = add(add(add(div(ref,ZL6),YL6sh),add(div(ref,ZL2),YL2sh)),div(ref,ZLD123));
 			         y[1][2] = add(div(refm,ZL6),div(refm,ZL2));
 			         y[1][3] = new Complex(0,0);
 			         y[1][4] = new Complex(0,0);
 			         y[2][0] = new Complex(0,0);
 			         y[2][1] = add(div(refm,ZL6),div(refm,ZL2));
-			         y[2][2] = add(add(add(add(div(ref,ZL6),div(ref,ZL6sh)),add(div(ref,ZL2),div(ref,ZL2sh))),div(ref,ZLD2)),div(ref,ZT2));
+			         y[2][2] = add(add(add(add(div(ref,ZL6),YL6sh),add(div(ref,ZL2),YL2sh)),div(ref,ZLD2)),div(ref,ZT2));
 			         y[2][3] = new Complex(0,0);
 			         y[2][4] = div(refm,ZT2);
 			         y[3][0] = new Complex(0,0);
@@ -228,21 +248,31 @@ public class Question3_calculation {
 			         y[4][2] = div(refm,ZT2);
 			         y[4][3] = div(refm,ZT3);
 			         y[4][4] = add(add(div(ref,ZT3),div(ref,ZT1)),add(div(ref,ZC1),div(ref,ZLD1)));
-
-			        
+			         
+			        Object[][] output = new Object [5][5];
 					String Y[][] = new String[5][5];
 					for(int p = 0 ; p < 5 ; p++){
 						for(int q = 0 ; q < 5 ; q++){
-							Y[p][q] = Double.toString(getReal(y[p][q]))+ "j*" + Double.toString(getImage(y[p][q]));
+							output[p][q] = Double.toString(getReal(y[p][q]))+ " + j*" + Double.toString(getImage(y[p][q]));
 							
 						}
 					}
+					String[] columnNames = {" ", " ", " ", " "," "};
+					JTable table2 = new JTable(output, columnNames);
+					JFrame jf2 = new JFrame();
+					jf2.setTitle("Admittance Matrix");
+					JScrollPane jsp2 = new JScrollPane(table2);
+					jf2.add(jsp2);
+					jf2.setBounds(0, 0, 500, 500);
+					jf2.setVisible(true);
+					/*
 					for (int p = 0 ; p < 5 ; p++){
 						for(int q = 0 ; q < 5 ; q++){
-							System.out.print(Y[p][q] + " ");
+							System.out.print(Y[p][q] + "          ");
 						}
 						System.out.print("\n");
-					}
+						
+					}*/
 					}
 					catch(Exception e){
 					e.printStackTrace();
